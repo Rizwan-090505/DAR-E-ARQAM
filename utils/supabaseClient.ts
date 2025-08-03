8ab36e7cb8388e89d0def8235581db4706e0afd1
Rizwan-090505 <rizzwanabdullah@gmail.com>
2025-08-03 19:53:54 +0500

    hardcoded

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // create an environment variable for the supabase url and the anon key on .env file
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = "https://tjdepqtouvbwqrakarkh.supabase.co";
+const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqZGVwcXRvdXZid3FyYWthcmtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxODM4NTMsImV4cCI6MjA2NDc1OTg1M30.5sippZdNYf3uLISBOHHlJkphtlJc_Q1ZRTzX9E8WYb8"
 
 console.log('Supabase URL:', supabaseUrl)
 console.log('Supabase Key (first 10 chars):', supabaseAnonKey?.substring(0, 10) + '...')
